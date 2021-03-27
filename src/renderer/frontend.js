3534c2ad9f70efa472ff42c57b33b81be5bba081
Fred Paucar <fred.paucarespinoza@gmail.com>
2021-03-27 13:15:46 -0500

    refactor code

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,76 +1,13 @@
-import url from 'url';
-import * as path from 'path';
-import applyFilter from './filters';
 import { openDirectory, setIpc } from './ipcRendererEvents';
+import { addImagesEvent, searchImagesEvent, selectEvent } from './images-ui';
 
 window.addEventListener('load', () => {
     setIpc();
-    addImagesEvents();
+    addImagesEvent();
     searchImagesEvent();
     selectEvent();
     buttonEvent('open-directory', openDirectory);
 });
-
-function addImagesEvents() {
-    const thumbs = document.querySelectorAll('li.list-group-item');
-
-    for (let i = 0; i < thumbs.length; i++) {
-        thumbs[i].addEventListener('click', function () {
-            changeImage(this);
-        });
-    }
-}
-
-function changeImage(nodo) {
-    let selected = document.querySelector('li.selected');
-    if (selected) {
-        selected.classList.remove('selected');
-    }
-    if (nodo) {
-        nodo.classList.add('selected');
-        document.getElementById('image-displayed').src = nodo.querySelector('img').src;
-    }
-}
-
-function searchImagesEvent() {
-    const searchBox = document.getElementById('search-box');
-
-    searchBox.addEventListener('keyup', function () {
-        const regex = new RegExp(this.value.toLowerCase(), 'gi');
-        const thumbs = document.querySelectorAll('li.list-group-item img');
-
-        if (this.value.length) {
-            for (let i = 0; i < thumbs.length; i++) {
-                const fileUrl = url.parse(thumbs[i].src);
-                const fileName = path.basename(fileUrl.pathname);
-
-                if (fileName.match(regex)) {
-                    thumbs[i].parentNode.classList.remove('hidden');
-                } else {
-                    thumbs[i].parentNode.classList.add('hidden');
-                }
-            }
-        } else {
-            for (let i = 0; i < thumbs.length; i++) {
-                thumbs[i].parentNode.classList.remove('hidden');
-            }
-        }
-        selectFirstImage();
-    });
-}
-
-function selectFirstImage() {
-    const image = document.querySelector('li.list-group-item:not(.hidden');
-    changeImage(image);
-}
-
-function selectEvent() {
-    const select = document.getElementById('filter_picker');
-
-    select.addEventListener('change', function () {
-        applyFilter(this.value, document.getElementById('image-displayed'));
-    })
-}
 
 function buttonEvent(id, func) {
     const buttonById = document.getElementById(id);
--- src/renderer/ipcRendererEvents.js
+++ src/renderer/ipcRendererEvents.js
@@ -1,10 +1,11 @@
 import { ipcRenderer } from 'electron';
+import { addImagesEvent, selectFirstImage } from './images-ui';
 
 function setIpc() {
     ipcRenderer.on('load-images', (event, images) => {
         clearImages();
         loadImages(images);
-        addImageEvent();
+        addImagesEvent();
         selectFirstImage();
     });
 }
@@ -33,32 +34,6 @@ function loadImages(images) {
 
         imagesList.insertAdjacentHTML('beforeend', imageTemplate);
     });
-}
-
-function addImageEvent() {
-    const thumbs = document.querySelectorAll('li.list-group-item');
-
-    for (let i = 0; i < thumbs.length; i++) {
-        thumbs[i].addEventListener('click', function () {
-            changeImage(this);
-        });
-    }
-}
-
-function changeImage(nodo) {
-    let selected = document.querySelector('li.selected');
-    if (selected) {
-        selected.classList.remove('selected');
-    }
-    if (nodo) {
-        nodo.classList.add('selected');
-        document.getElementById('image-displayed').src = nodo.querySelector('img').src;
-    }
-}
-
-function selectFirstImage() {
-    const image = document.querySelector('li.list-group-item:not(.hidden');
-    changeImage(image);
 }
 
 module.exports = {
